6eba8ed8be004c3d6d28a755062a127911c58c98
Pasidu Ranasinghe <pasinduprabath78@gmail.com>
2023-12-25 11:46:39 +0530

    add basic pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+import Login from './pages/login';
+import Register from './pages/Register';
+import About from './pages/about';
 
 export default function App() {
   return (
-    <div className='text-red-500'><h1>hello</h1></div>
-  )
+        <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/about" element={<About />} />
+        </Routes>
+        </BrowserRouter>
+    )
 }
