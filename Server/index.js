f2456de50d8110e026f03834420019a57a144491
Pasidu Ranasinghe <pasinduprabath78@gmail.com>
2023-12-30 20:12:31 +0530

    Changes : updates the server with routes, controllers and models fixed #1

--- Server/index.js
+++ Server/index.js
@@ -1,10 +1,12 @@
 import  dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{
-    console.log("MongoDB connented");
+    console.log("MongoDB Connected");
 }).catch((err)=>{
     console.log(err);
 })
@@ -14,3 +16,6 @@ const app = express();
 app.listen(3000, () =>{
         console.log("Server is running on port 3000");
 });
+
+app.use('/api/v1/user', userRouter);
+app.use('/api/v1/auth', authRouter);
