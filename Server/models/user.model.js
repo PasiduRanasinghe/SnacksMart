7dd9e82698c9cd5c9e71dc135bbd4bbc7613dfb4
Pasidu Ranasinghe <pasinduprabath78@gmail.com>
2023-12-31 23:07:21 +0530

    fixed user.model : fixed syntax errors

--- Server/models/user.model.js
+++ Server/models/user.model.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
-new userschema = new mongoose.Schema({
+
+const userSchema = new mongoose.Schema({
     userName: {
         type : String,
         required: true,
@@ -20,6 +21,6 @@ new userschema = new mongoose.Schema({
 
 }, {timestamps :true});
 
-const User = mongoose.model(User, userschema);
+const User = mongoose.model('User', userSchema);
 
 export default User;
