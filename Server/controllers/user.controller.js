e4da7ed8354a889c5bc18f9724f442b8afee71ba
PasiduRanasinghe <prabhathranasinghe0@gmail.com>
2024-01-08 13:30:32 +0530

    create update user api route

--- Server/controllers/user.controller.js
+++ Server/controllers/user.controller.js
@@ -1,7 +1,32 @@
-const test = (req, res)=> {
-    res.json({
-        message: 'Api route is working',
-    });
-}
+import { errorHandler } from '../utils/error.js';
+import User from './../models/user.model.js';
 
-export {test}
+const test = (req, res) => {
+  res.json({
+    message: 'Api route is working',
+  });
+};
+const updateUser = async (req, res, next) => {
+  if (req.user.id != req.params.id)
+    return next(errorHandler(401, 'you can only update your own account!'));
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          userName: req.body.username,
+          email: req.body.email,
+          avatar: req.body.avatar,
+        },
+      },
+      { new: true }
+    );
+    const { password, ...rest } = updatedUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
+export { test, updateUser };
--- Server/index.js
+++ Server/index.js
@@ -1,15 +1,16 @@
-import dotenv from "dotenv";
-import express from "express";
-import mongoose from "mongoose";
-import userRouter from "./routes/user.route.js";
-import authRouter from "./routes/auth.route.js";
+import dotenv from 'dotenv';
+import express from 'express';
+import mongoose from 'mongoose';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 //connect to mongoDB
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
-    console.log("MongoDB Connected");
+    console.log('MongoDB Connected');
   })
   .catch((err) => {
     console.log(err);
@@ -19,20 +20,21 @@ mongoose
 const app = express();
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log('Server is running on port 3000');
 });
 
-//middleware json support
+//middleware
 app.use(express.json());
+app.use(cookieParser());
 
 //redirect routes
-app.use("/api/v1/user", userRouter);
-app.use("/api/v1/auth", authRouter);
+app.use('/api/v1/user', userRouter);
+app.use('/api/v1/auth', authRouter);
 
 //middleware for error handling
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || "Internal Server Error";
+  const message = err.message || 'Internal Server Error';
   return res.status(statusCode).json({
     success: false,
     statusCode,
