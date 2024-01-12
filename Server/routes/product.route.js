efd9b12c04c08f3f59b11374f18e777b3b119211
PasiduRanasinghe <prabhathranasinghe0@gmail.com>
2024-01-12 15:11:47 +0530

    complete get product server function

--- Server/routes/product.route.js
+++ Server/routes/product.route.js
@@ -1,9 +1,13 @@
 import express from 'express';
-import { createProduct } from '../controllers/product.controller.js';
+import {
+  createProduct,
+  getAllProducts,
+} from '../controllers/product.controller.js';
 import { verifyToken } from './../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, createProduct);
+router.get('/list', getAllProducts);
 
 export default router;
