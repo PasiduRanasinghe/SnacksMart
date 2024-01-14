ccdd8dfd850f2668f797bedd6d779cd38d391218
PasiduRanasinghe <prabhathranasinghe0@gmail.com>
2024-01-14 10:02:42 +0530

    Revert "complete get product server function"
    This reverts commit efd9b12c04c08f3f59b11374f18e777b3b119211.

--- Server/routes/product.route.js
+++ Server/routes/product.route.js
@@ -1,13 +1,9 @@
 import express from 'express';
-import {
-  createProduct,
-  getAllProducts,
-} from '../controllers/product.controller.js';
+import { createProduct } from '../controllers/product.controller.js';
 import { verifyToken } from './../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, createProduct);
-router.get('/list', getAllProducts);
 
 export default router;
