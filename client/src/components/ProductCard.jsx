cb5546a93fe90f55932961f5b7f108d46ec88312
PasiduRanasinghe <prabhathranasinghe0@gmail.com>
2024-01-29 23:28:48 +0530

    add product card and home ui

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -7,28 +7,22 @@ import {
   Button,
 } from '@material-tailwind/react';
 
-export default function ProductCard(props) {
+export default function ProductCard({ product }) {
   return (
     <Card className="mt-6 w-64 h-80">
       <CardHeader color="blue-gray" floated={false} className="m-2 shadow-none">
-        <img
-          src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
-          alt="card-image"
-        />
+        <img src={product.image} alt="card-image" />
       </CardHeader>
-      <CardBody className="m-0 p-2">
-        <Typography variant="h6" color="blue-gray" className="mb-2">
-          UI/UX Review Check
+      <CardBody className="m-0 p-2 flex flex-col">
+        <Typography variant="h5" color="blue-gray" className="mb-2">
+          {product.title}
         </Typography>
-        <Typography variant="small">
-          The place is close to Barceloneta Beach and bus stop just 2 min by
+        <Typography variant="small">{product.description}</Typography>
+        <Typography className=" self-end font-bold" variant="medium">
+          LKR {product.price}
         </Typography>
       </CardBody>
-      <CardFooter className="p-2 flex justify-evenly">
-        <Button size="sm" className="">
-          buy now
-        </Button>
-
+      <CardFooter className="p-2 flex justify-center">
         <Button size="sm">add cart</Button>
       </CardFooter>
     </Card>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import { Carousel } from '@material-tailwind/react';
+import ProductCard from './../components/ProductCard';
+import { toast } from 'react-toastify';
 
 export default function Home() {
+  const [productList, setProductList] = useState([]);
+  useEffect(() => {
+    const handleProducts = async () => {
+      const res = await fetch('/api/v1/product/list');
+      const data = await res.json();
+      if (data.success === false) {
+        toast.error(data.message);
+        return;
+      }
+      setProductList(data);
+    };
+
+    handleProducts();
+  }, []);
   return (
     <div>
-      <h1>Home</h1>
+      <Carousel autoplay={true} loop={true} className=" h-80">
+        <img
+          src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
+          alt="image 1"
+          className="h-full w-full object-cover"
+        />
+        <img
+          src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
+          alt="image 2"
+          className="h-full w-full object-cover"
+        />
+      </Carousel>
+      <div className=" mx-5 justify-items-center grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  ">
+        {productList &&
+          productList.length > 0 &&
+          productList.map((product) => (
+            <ProductCard key={product._id} product={product} />
+          ))}
+      </div>
     </div>
   );
 }
