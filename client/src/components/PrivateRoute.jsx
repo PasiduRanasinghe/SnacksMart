fb0d312d8f08bacee8f41151b112ed79cebb6a82
PasiduRanasinghe <prabhathranasinghe0@gmail.com>
2024-02-05 20:32:56 +0530

    bugs fixed on private route

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -1,33 +1,34 @@
 import React, { useEffect, useState } from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
+import { Navigate, Outlet, useNavigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import axios from '../api/axiosInstance';
 import { toast } from 'react-toastify';
 import { Spinner } from '@material-tailwind/react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { removeUser } from '../redux/slices/userSlice';
 
 export default function PrivateRoute({ role }) {
   const [userRole, setUserRole] = useState(null);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchProduct = async () => {
       try {
         const res = await axios.get('/auth/authenticate');
         const data = res.data;
-
-        if (data.success === false) {
-          setUserRole('error');
-          dispatch(removeUser());
-          toast.error(data.message);
-          return;
-        }
-
         setUserRole(data.role);
       } catch (error) {
-        toast.error(error.message);
+        if (error.response && error.response.status === 401) {
+          setUserRole('error');
+          dispatch(removeUser());
+          console.log(error.response);
+          toast.error(error.response.statusText);
+          return;
+        } else {
+          toast.error(error.message);
+        }
       }
     };
 
