f680839bec0a4dea067a673467a2450c90af0140
PasiduRanasinghe <prabhathranasinghe0@gmail.com>
2024-02-05 20:31:43 +0530

    update unauthorized page

--- client/src/pages/Unauthorized.jsx
+++ client/src/pages/Unauthorized.jsx
@@ -1,12 +1,21 @@
-import { Typography } from '@material-tailwind/react';
+import { Button, Typography } from '@material-tailwind/react';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Unauthorized() {
   return (
-    <div className=" w-full h-full">
+    <div className=" flex  flex-col justify-center items-center w-screen h-96">
       <Typography variant="h3" color="red">
-        Unauthorized
+        401 - Unauthorized
       </Typography>
+      <Typography variant="h6" color="red" className="my-4">
+        You are not authorized to access this page
+      </Typography>
+      <Link to="/">
+        <Button variant="outlined" color="blue">
+          RETURN Home
+        </Button>
+      </Link>
     </div>
   );
 }
